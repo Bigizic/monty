ada98277133d7e9d15f88ea7afc077101c323a01
Bigizic <olalekanisaac75@gmail.com>
2023-05-25 23:29:22 +0000

    Add monty.h

--- monty.h
+++ monty.h
@@ -71,7 +71,7 @@ extern store_t bus;
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
-
+void _pop(stack_t **stack, unsigned int line_number);
 
 
 
