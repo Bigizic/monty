80dfc049b72354f2d3028ad9a88e2e3b7142bab6
Bigizic <olalekanisaac75@gmail.com>
2023-05-25 23:53:57 +0000

    Add nop.c

---
+++ nop.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/* header */
+
+/**
+* _nop - do nothing
+*
+* @stack: head
+*
+* @line_number: counter
+*
+* Return: void
+*/
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}	
