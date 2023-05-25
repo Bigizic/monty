0bf57484ef8186d63e50095c8e05efdde0d438c2
Bigizic <olalekanisaac75@gmail.com>
2023-05-25 00:32:22 +0000

    Add main.c

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
 /* header */
 
 /**
@@ -13,31 +15,31 @@
 int main(int ac, char *av[])
 {
 	unsigned int line_number = 1;
-	char *buffer = NULL, *string = NULL;
+	char *buffer = NULL;
 	stack_t *stack_cp = NULL;
 	FILE *read_stream = NULL;
-	size_t buffer_len 0;
+	size_t buffer_len = 0;
 
 	if (ac != 2)
 	{
-		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	read_stream = fopen(av[1], "r");
 	if (read_stream == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", av[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&buffer, &buffer_len, read_stream) != -1)
 	{
 		if (buffer != NULL)
-			_opcodes(&stack, line_number, buffer);
+			_opcodes(&stack_cp, line_number, buffer);
 		line_number++;
 	}
-	free_stack(stack);
+	free_stack(stack_cp);
 
-	stack = NULL;
+	stack_cp = NULL;
 	fclose(read_stream);
 	exit(EXIT_SUCCESS);
 }
@@ -57,19 +59,19 @@ void _opcodes(stack_t **stack, unsigned int line_number, char *buffer)
 
 	instruction_t my_opcodes[] = {
 		{"push", _push},
-		{NULL, NULL};
-	}
+		{NULL, NULL}
+	};
 
 	while (my_opcodes[i].opcode)
 	{
 		if (strcmp(my_opcodes[i].opcode, buffer) == 0)
 		{
-			my_opcode[i].f(stack, line_number);
+			my_opcodes[i].f(stack, line_number);
 			return;
 		}
 		i++;
 	}
-	dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, code);
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, buffer);
 	free_stack(*stack);
 	exit(EXIT_FAILURE);
 }
