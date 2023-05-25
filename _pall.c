4105820c5706b9a139b4f97a507ac709238630bd
Bigizic <olalekanisaac75@gmail.com>
2023-05-25 00:41:24 +0000

    Add _pall.c

---
+++ _pall.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/* header */
+
+/**
+* _pall - prints the values on the stack, starting from top
+*
+* @stack: double pointer to the head of list
+*
+* @line_number: number of line
+*
+* Return: void
+*/
+void _pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current_stack;
+
+	(void) line_number;
+
+	current_stack = *stack;
+
+	while (current_stack != NULL)
+	{
+		fprintf(stdout,"%d\n", current_stack->n);
+		current_stack = current_stack->next;
+	}
+}
