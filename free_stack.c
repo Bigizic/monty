1d7c31d9095c56a5ca7a99510b1f6a444b8da51c
Bigizic <olalekanisaac75@gmail.com>
2023-05-25 00:07:30 +0000

    Add free_stack.c

---
+++ free_stack.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/* header */
+
+/**
+* free_stack - frees stack list
+*
+* @stack: list to be freed
+*
+* Return: void
+*/
+void free_stack(stack_t *stack)
+{
+	stack_t *current_stack;
+
+	current_stack = stack;
+
+	while (current_stack != NULL)
+	{
+		current_stack = stack->next;
+		free(stack);
+		stack = current_stack;
+	}
+}
