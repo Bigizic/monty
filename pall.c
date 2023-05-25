6f6218aabcb73a64a62f60bbe2e927f864ac32d5
Bigizic <olalekanisaac75@gmail.com>
2023-05-25 11:47:42 +0000

    Add pall.c

---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+* _pall - prints stack n
+*
+* @stack: pointer to head
+*
+* @line_number: line number for error
+*
+* Return: void
+*/
+void _pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
