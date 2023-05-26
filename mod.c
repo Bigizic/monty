489795e66320b6a6a6f9605aaa9b0840da73a5f4
Bigizic <olalekanisaac75@gmail.com>
2023-05-26 10:34:28 +0000

    Add mod.c

--- mod.c
+++ mod.c
@@ -15,9 +15,18 @@
 void _mod(stack_t **stack, unsigned int line_number)
 {
 	stack_t *h;
-	int len = 0, x;
+	int len = 0;
 
 	h = *stack;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		free(bus.file);
+		free(bus.buffer);
+		_free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
 	while (h)
 	{
 		h = h->next;
@@ -31,6 +40,7 @@ void _mod(stack_t **stack, unsigned int line_number)
 		_free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
+	h = *stack;
 	if (h->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
@@ -40,8 +50,8 @@ void _mod(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	x = h->next->n % h->n;
-	h->next->n = x;
+	h->next->n = h->n % h->next->n;
 	*stack = h->next;
 	free(h);
+	h->prev = NULL;
 }
