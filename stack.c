9b9e00555ef41b8eb4867c12861063963f766bb1
Bigizic <olalekanisaac75@gmail.com>
2023-05-26 11:26:03 +0000

    Add stack.c

---
+++ stack.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/* header */
+
+/**
+* _stack -  sets the format of the data to a stack (LIFO)
+* This is the default behavior of the program
+*
+* @stack: head
+*
+* @line_number: counter
+*
+* Return: void
+*/
+
+void _stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+
+	bus.lifi = 0;
+}
