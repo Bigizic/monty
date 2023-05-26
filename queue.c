ed33009a65d057fad7accd4e2c0d1e6373d63490
Bigizic <olalekanisaac75@gmail.com>
2023-05-26 11:26:16 +0000

    Add queue.c

---
+++ queue.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/* header */
+
+/**
+* _queue - sets the format of the data to a queue (FIFO)
+*
+* @stack: head
+*
+* @line_number: counter
+*
+* Return: void
+*/
+
+void _queue(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+
+	bus.lifi = 1;
+}
