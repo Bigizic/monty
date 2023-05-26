f7974261406dcd398041144604abc421735b13a3
Bigizic <olalekanisaac75@gmail.com>
2023-05-26 10:47:10 +0000

    Add pstr.c

---
+++ pstr.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/* header */
+
+/**
+* _pstr -  prints the string starting at the top of the stack,
+* followed by a new line. integer stored is treated as ASCII value
+*
+* @stack: head
+*
+* @line_number: counter
+*
+* Return: void
+* */
+
+void _pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h;
+
+	(void)line_number;
+
+	h = *stack;
+
+	while (h)
+	{
+		if (h->n > 127 || h-> <= 0)
+			break;
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
